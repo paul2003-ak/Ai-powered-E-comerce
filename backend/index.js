aaaefb8963115f21166ecee0e9b5a337bf8fd961
paul2003-ak <bablijoy08@gmail.com>
2025-07-29 14:47:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app=express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173" , "http://localhost:5174"],
+    origin:["https://ai-powered-e-comerce-frontend-onecart.onrender.com" , "http://localhost:5174"],
     credentials:true
 }))
 
