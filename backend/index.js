cbefd2e49ff0755732e40af1f4533249eac3a786
paul2003-ak <bablijoy08@gmail.com>
2025-07-29 12:52:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app=express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://ai-powered-e-comerce-frontend-one.onrender.com" , "http://localhost:5174"],
+    origin:["https://ai-powered-e-comerce-frontend-one.onrender.com" , "https://ai-powered-e-comerce-admin.onrender.com"],
     credentials:true
 }))
 
